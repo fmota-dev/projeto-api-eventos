6b3316b141cc6b159b78d3488313678f4d8fa3cb
@fmota-dev <fmota.web@gmail.com>
2024-05-26 23:54:16 -0300

    corrigindo format data no list front

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,14 @@
 const BASE_URL = 'https://projeto-api-eventos.vercel.app';
 
+// Função para formatar a data no formato DD/MM/YYYY
+function formatarData(data) {
+	const dataObj = new Date(data);
+	const dia = String(dataObj.getUTCDate()).padStart(2, '0');
+	const mes = String(dataObj.getUTCMonth() + 1).padStart(2, '0');
+	const ano = dataObj.getUTCFullYear();
+	return `${dia}/${mes}/${ano}`;
+}
+
 fetch(`${BASE_URL}/eventos`)
 	.then((response) => response.json())
 	.then((eventos) => {
@@ -7,11 +16,13 @@ fetch(`${BASE_URL}/eventos`)
 		eventos.forEach((evento) => {
 			const row = document.createElement('tr');
 			row.innerHTML = `
-            <td>${evento.titulo}</td>
-            <td>${evento.descricao}</td>
-            <td>${evento.data}</td>
-            <td>${evento.local}</td>
-          `;
+                <td>${evento.titulo}</td>
+                <td>${evento.descricao}</td>
+                <td>${formatarData(
+									evento.data
+								)}</td> // Formata a data antes de exibi-la na tabela
+                <td>${evento.local}</td>
+            `;
 			tbody.appendChild(row);
 		});
 	});
